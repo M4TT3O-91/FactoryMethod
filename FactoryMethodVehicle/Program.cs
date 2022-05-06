b02def1be9a729c617da9db52c61989bb777c7b9
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-06 10:32:22 +0200

    initial commit

---
+++ FactoryMethodVehicle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactoryMethodVehicle
+{
+    class Program
+    {
+        static void Main()
+        {
+            var vehicleWith4wheels = VehicleFactory.Build(4,2000);
+            Console.WriteLine($"I'm a ... {vehicleWith4wheels.GetType()}");
+
+
+            var vehicleWith2wheels = VehicleFactory.Build(2,50);
+            Console.WriteLine($"I'm a ... {vehicleWith2wheels.GetType()} and my power is {vehicleWith2wheels.GetPower()}");
+
+            var vehicleWith2wheels2 = VehicleFactory.Build(2, 150);
+            Console.WriteLine($"I'm a ... {vehicleWith2wheels2.GetType()}");
+
+            var vehicleWith6wheels = VehicleFactory.Build(6, 6000);
+            Console.WriteLine($"I'm a ... {vehicleWith6wheels.GetType()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FactoryMethodVehicle/VehicleFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace FactoryMethodVehicle
+{
+    public class VehicleFactory
+    {
+        public static IVehicle Build(int numberOfWheels, int cilindrata) => numberOfWheels switch
+        {
+            2 => cilindrata > 125 ? new Motorbike(cilindrata) : new Scooter(cilindrata),
+            4 => new Car(cilindrata),
+            6 => new Truck(cilindrata),
+            _ => throw new NumberOfWheelsNotSupported()
+        };
+    }
+}
